90089905a6b7a6361ba87c7911c1483f34d880a8
josh <jbothun@concentricsky.com>
2012-03-21 13:38:13 -0700

    update live config

--- apps/mainsite/settings_production.py
+++ apps/mainsite/settings_production.py
@@ -1,6 +1,8 @@
 from settings import *
 from mainsite import TOP_DIR
 
+import os
+
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 TASTYPIE_FULL_DEBUG = DEBUG
@@ -8,14 +10,17 @@ TASTYPIE_FULL_DEBUG = DEBUG
 TIME_ZONE = 'America/Los_Angeles'
 LANGUAGE_CODE = 'en-us'
 
-STATIC_URL = '//minornine.com/heroku/mealspace/static/'
+STATIC_URL = os.getenv('MY_STATIC_URL')
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'mealspace',                 # Or path to database file if using sqlite3.
-        'USER': 'root',
-        'PASSWORD': '',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mealspace',
+
+        'USER':     os.getenv('MY_DB_USER'),
+        'PASSWORD': os.getenv('MY_DB_PASS'),
+        'HOST':     os.getenv('MY_DB_HOST'),
+        # 'PORT':     os.getenv('MY_DB_PORT'),
     }
 }
 
