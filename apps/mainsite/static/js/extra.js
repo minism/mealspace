e34a936296b835e188a5692a4c72957c90a7d004
josh <jbothun@concentricsky.com>
2012-03-24 12:41:09 -0700

    add search clear button

--- apps/mainsite/static/js/extra.js
+++ apps/mainsite/static/js/extra.js
@@ -3,4 +3,8 @@ $(function()
     $('.main-error > a.close').click(function(e) {
         $(this).parent('.alert').hide();
     });
+
+    $('#clear-search').click(function() {
+        $('#search').val('').focus();
+    })
 })
