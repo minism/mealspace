54212008560ccd406cdee7b467f7e274d3038ab9
josh <jbothun@concentricsky.com>
2012-03-24 12:49:57 -0700

    add search filtering

--- apps/mainsite/static/js/extra.js
+++ apps/mainsite/static/js/extra.js
@@ -6,5 +6,22 @@ $(function()
 
     $('#clear-search').click(function() {
         $('#search').val('').focus();
-    })
+        $('#ingredient-list li').show()
+    });
+
+    $('#search').keyup(function(e) {
+        var text = $(this).val();
+        var re = new RegExp(text, 'i');
+        $('#ingredient-list li').each(function(){
+            var ingredient_name = $(this).find('.ingredient-name').html()
+            console.log(ingredient_name);
+            if($.trim(ingredient_name).search(re) == -1) {
+                $(this).hide()
+            }   
+            else {
+                $(this).show()
+            }
+        });
+    });
+
 })
